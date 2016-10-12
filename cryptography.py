974c42671e930f92aae47beeb75c1fa1c2186649
DRBarnum <douglasbarnum@hanovernorwichschools.org>
2016-10-12 13:17:02 -0400

    Updated from Brython Server: 10/12/2016 1:17:01 PM

--- cryptography.py
+++ cryptography.py
@@ -11,9 +11,9 @@ See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptog
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 
-
 a=[]
 b=[]
+f=[]
 Coding=True
 while Coding:
     start=input("Enter e to encrypt, d to decrypt, or q to quit: ")
@@ -24,16 +24,21 @@ while Coding:
         encryption=input("Mesage: ")
         for c in encryption:
             n=(associations.find(c))
-            print(n)
+            print(n) # Prints Message Numbers DELETE THIS
             a.append(n)
         key=input("Key: ")
         for c in key:
             m=(associations.find(c))
-            print(m)
+            print(m) # Prints Key Numbers DELETE THIS
             b.append(m)
+        p=b*10
         for l in range(len(encryption)):
-            q=a[l]+b[l]
-            print("Outcome", q)
+            q=a[l]+p[l]
+            f.append(q)
+        for h in f:
+            k=associations[h]
+            print(k, end="")
+        print()
     elif start == "d":
         decryption=input("Message: ")
         key=input("Key: ")
